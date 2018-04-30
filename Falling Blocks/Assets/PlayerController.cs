211bef37c68fde247c8d2e273f9150424cd9d1d7
Fluffisaurus <angushon96@gmail.com>
2018-04-29 17:45:19 -0700

    added movement

---
+++ Falling Blocks/Assets/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 5;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float inputX = Input.GetAxisRaw("Horizontal");
+        float velocity = inputX * speed;
+        transform.Translate (Vector2.right * velocity * Time.deltaTime);
+	}
+}
